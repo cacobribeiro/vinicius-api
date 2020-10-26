9854f3ad981fbe3df67923f5fcd3b0d8dc3fa0d9
Caio Ribeiro <cacoribeiro@gmail.com>
2020-10-26 14:40:49 -0300

    Updates

---
+++ precache-manifest.bc80d9136359bd539e11f1e560106b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33a2fe3f5ddfd260592b9a3209a854f",
+    "url": "/vinicius-api/index.html"
+  },
+  {
+    "revision": "c91fc45014e9dffb5cf5",
+    "url": "/vinicius-api/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "416fb695d4c51c0869e6",
+    "url": "/vinicius-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c91fc45014e9dffb5cf5",
+    "url": "/vinicius-api/static/js/2.76aa3b4f.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/vinicius-api/static/js/2.76aa3b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416fb695d4c51c0869e6",
+    "url": "/vinicius-api/static/js/main.d9b74e42.chunk.js"
+  },
+  {
+    "revision": "4c5c96bea696a8d64922",
+    "url": "/vinicius-api/static/js/runtime-main.fec13964.js"
+  }
+]);
